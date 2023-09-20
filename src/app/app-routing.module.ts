d435f0eb2cf281c0c577565b55e0693c2839b910
EnvyL27 <farrel.ahmad.zaidan@gmail.com>
2023-09-20 15:50:06 +0700

    add change page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SkriningComponent } from './skrining/skrining.component';
+import { TestComponent } from './test/test.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'scrining', component: SkriningComponent},
+  {path: 'hasil', component: TestComponent},
 ];
 
 @NgModule({
--- src/app/skrining/skrining.component.ts
+++ src/app/skrining/skrining.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./skrining.component.css']
 })
 export class SkriningComponent {
+datadiri: boolean = true;
+skrining: boolean = false;
 
+ddChange(){
+  this.skrining = false
+  this.datadiri = !this.datadiri
+  console.log(this.datadiri);
+  
+}
+skringingChange(){
+  this.skrining = !this.skrining
+  this.datadiri = false
+  console.log(this.skrining);
+  
+}
 }
