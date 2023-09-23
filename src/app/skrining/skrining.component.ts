30d3ba2d74a983a450efeb66f609d297f76c1b24
uglysquid27 <farrel.ahmad.zaidan@gmail.com>
2023-09-23 20:04:23 +0700

    updates on skrining page

--- src/app/skrining/skrining.component.ts
+++ src/app/skrining/skrining.component.ts
@@ -1,24 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-skrining',
   templateUrl: './skrining.component.html',
   styleUrls: ['./skrining.component.css']
 })
-export class SkriningComponent {
-datadiri: boolean = true;
-skrining: boolean = false;
+export class SkriningComponent implements OnInit {
 
-ddChange(){
-  this.skrining = false
-  this.datadiri = !this.datadiri
-  console.log(this.datadiri);
-  
-}
-skringingChange(){
-  this.skrining = !this.skrining
-  this.datadiri = false
-  console.log(this.skrining);
-  
-}
+  ngOnInit(): void {
+  }
+  datadiri: boolean = true;
+  skrining: boolean = false;
+
+  ddChange() {
+    this.skrining = false
+    if (this.datadiri == false) {
+      this.datadiri = !this.datadiri
+    }
+
+  }
+  skringingChange() {
+    this.datadiri = false
+    if (this.skrining == false) {
+      this.skrining = !this.skrining
+    }
+  }
 }
